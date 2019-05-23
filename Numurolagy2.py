c8682dbd5ab5ce42198cd3396f7fb2c35db0697b
Mugunthan <37899932+Mugunthan1990@users.noreply.github.com>
2019-05-23 09:36:48 +0530

    Add files via upload

---
+++ Numurolagy2.py
@@ -0,0 +1,28 @@
+
+lt = {'a':1, 'q':1,'y':1, 'i':1, 'j' : 1,
+      'b':2, 'r':2, 'k':2,
+      'g':3, 'c':3, 'l':3, 's':3,
+      'd':4, 'm':4,'t':4,
+      'e':5, 'h':5, 'n':5, 'x':5,
+      'u':6, 'v':6, 'w':6,
+      'o':7, 'z':7,
+      'f':8, 'p':8
+      }
+#name = input("Enter Your Name: ")
+sum = 0
+name = [
+"Sandesh",
+"Sanjeev",
+"Sanjiv",
+"SenTamilChelvan",
+"Shailesh",
+"Sharan",
+"Shreeharsh"
+]
+sum = 0
+for na in name:
+    for item in na.lower():
+        #print(item)
+        sum += lt[item]
+    print("{0} =  {1}".format(na,sum))
+    sum = 0
